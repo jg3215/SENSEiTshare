51e2f877a1ec1b964b3feb762ea5e9a2dc03214e
Artur112 <36160815+Artur112@users.noreply.github.com>
2018-06-15 16:08:23 +0100

    showing costa github ting

--- SenseiT/app/src/main/java/com/example/costa/senseit/MainActivity.java
+++ SenseiT/app/src/main/java/com/example/costa/senseit/MainActivity.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
