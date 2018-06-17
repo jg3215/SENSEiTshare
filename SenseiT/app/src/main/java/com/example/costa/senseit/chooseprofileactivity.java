6a17688f45f6d5bccd344ffecf1e179356353f11
copa977 <36160802+copa977@users.noreply.github.com>
2018-06-17 01:11:01 +0100

    rnnn

--- SenseiT/app/src/main/java/com/example/costa/senseit/chooseprofileactivity.java
+++ SenseiT/app/src/main/java/com/example/costa/senseit/chooseprofileactivity.java
@@ -1,13 +1,103 @@
 package com.example.costa.senseit;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class chooseprofileactivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chooseprofile);
+        File directory = getExternalFilesDir("/Profiles/");
+        File[] files = directory.listFiles();
+            TextView text_user1 = (TextView) findViewById(R.id.text_user1);
+            TextView text_user2 = (TextView) findViewById(R.id.text_user2);
+            TextView text_user3 = (TextView) findViewById(R.id.text_user3);
+            TextView text_user4 = (TextView) findViewById(R.id.text_user4);
+        if (files.length==0)
+         {  String u1="Add a User";
+             String u2="Add a User";
+             String u3="Add a User";
+             String u4="Add a User";
+             text_user1.setText(u1);
+             text_user2.setText(u2);
+             text_user3.setText(u3);
+             text_user4.setText(u4);
+
+         }
+        else if (files.length==1)
+        {  String u1= files[0].getName();
+           u1= u1.replace(".txt", "");
+            String u2="Add a User";
+            String u3="Add a User";
+            String u4="Add a User";
+            text_user1.setText(u1);
+            text_user2.setText(u2);
+            text_user3.setText(u3);
+            text_user4.setText(u4);
+        }
+        else if (files.length==2)
+        {  String u1= files[0].getName();
+            u1= u1.replace(".txt", "");
+            String u2= files[1].getName();
+            u2= u2.replace(".txt", "");
+            String u3="Add a User";
+            String u4="Add a User";
+            text_user1.setText(u1);
+            text_user2.setText(u2);
+            text_user3.setText(u3);
+            text_user4.setText(u4);
+        }
+        else if (files.length==3)
+        {  String u1= files[0].getName();
+            String u2= files[1].getName();
+            String u3=files[2].getName();
+            u1= u1.replace(".txt", "");
+            u2= u2.replace(".txt", "");
+            u3= u3.replace(".txt", "");
+            String u4="Add a User";
+            text_user1.setText(u1);
+            text_user2.setText(u2);
+            text_user3.setText(u3);
+            text_user4.setText(u4);
+        }
+        else if (files.length==4)
+        {  String u1= files[0].getName();
+            String u2= files[1].getName();
+            String u3=files[2].getName();
+            String u4=files[3].getName();
+            u1= u1.replace(".txt", "");
+            u2= u2.replace(".txt", "");
+            u3= u3.replace(".txt", "");
+            u4= u4.replace(".txt", "");
+            text_user1.setText(u1);
+            text_user2.setText(u2);
+            text_user3.setText(u3);
+            text_user4.setText(u4);
+        }
+        else{
+            String u1= files[0].getName();
+            String u2= files[1].getName();
+            String u3=files[2].getName();
+            String u4=files[3].getName();
+            u1= u1.replace(".txt", "");
+            u2= u2.replace(".txt", "");
+            u3= u3.replace(".txt", "");
+            u4= u4.replace(".txt", "");
+            text_user1.setText(u1);
+            text_user2.setText(u2);
+            text_user3.setText(u3);
+            text_user4.setText(u4);
+        }
+
     }
 }
--- SenseiT/app/src/main/java/com/example/costa/senseit/createprofileactivity.java
+++ SenseiT/app/src/main/java/com/example/costa/senseit/createprofileactivity.java
@@ -4,17 +4,35 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import java.io.File;
+import java.io.IOException;
 
 public class createprofileactivity extends AppCompatActivity {
-
+    EditText NewUserName;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.createprofile);
-    }
+        NewUserName = (EditText) findViewById(R.id.editText2);
+
+
+        }
     public void crprclick(View v)
     {
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        Intent intent = new Intent(getApplicationContext(), chooseprofileactivity.class);
+        String user_name = NewUserName.getText().toString();
+
+        File directory = getExternalFilesDir("/Profiles/");
+        File file_user = new File(directory,user_name+".txt");
+        try {
+            file_user.createNewFile();
+
+        } catch (IOException e){}
+
+
         startActivity(intent);
     }
 }
